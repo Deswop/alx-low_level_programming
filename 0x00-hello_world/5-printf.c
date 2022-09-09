fd058cc800429b4553e601385acebec8058e7da1
Deswop <amuadh047@gmail.com>
2022-09-08 21:27:31 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing using printf
+ * Description: Printing a string using printf followed by a new line 
+ * Return: 0
+ */
+int main(void)
+{ printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
