f677c30a7cf51ef1d805bf3e3ed2954a7f87be05
Deswop <amuadh047@gmail.com>
2022-09-08 20:59:19 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing using put function
+ *
+ * Return: Always set as 0
+ *
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+
+	Return(0);
+
+}
