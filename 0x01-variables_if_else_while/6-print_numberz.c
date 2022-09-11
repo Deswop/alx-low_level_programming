e618bc329bcc00a850733019f049884bafa9e39e
Deswop <amuadh047@gmail.com>
2022-09-11 04:51:41 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - printing numbers using putchar
+ *
+ * Description: printing single digit numbers using putchar function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c < '10'; c++)
+	{
+		putchar(c)
+	}
+	putchar("\n");
+	return (0);
+}
