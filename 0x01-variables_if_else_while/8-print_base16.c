38321f28d8c356096bac3132d32cc22527aa2ffd
Deswop <amuadh047@gmail.com>
2022-09-11 05:36:40 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - printing hexadecimals 
+ *
+ * Description: printing hexadecimals in lower case the a line
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num <10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
