5371d95a9ae27eadad3677336e218bc03ca11e01
Deswop <amuadh047@gmail.com>
2022-09-11 04:37:34 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - printing numbers
+ *
+ * Description: printing single numbers in base ten
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i\n", i);
+	}
+	return (0);
+}
