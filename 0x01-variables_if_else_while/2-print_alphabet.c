a9f339e7be06e7ed2b929a4b63a685107930e710
Deswop <amuadh047@gmail.com>
2022-09-10 19:42:30 -0700

    printing alphabetas a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing alphabets in lowercase
+ *
+ * Description: write a program that prints all alphabets
+ * in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c ="a" ; c <= "z" ; ch++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}
