d1beeb8e9b4f0ffe77d5900b9d3071ce7ebe7dc8
Deswop <amuadh047@gmail.com>
2022-09-13 22:39:56 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,17 +5,13 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void);
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char ch;
 
-	return (0);
+	for (ch >= 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
