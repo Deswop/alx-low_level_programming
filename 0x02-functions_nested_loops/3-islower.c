202a8eac3ed536cba657da989bda3f5b73855f7b
Deswop <amuadh047@gmail.com>
2022-09-13 20:52:07 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a program that checks for lower case
+ * @c: a character inputed
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <='z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
