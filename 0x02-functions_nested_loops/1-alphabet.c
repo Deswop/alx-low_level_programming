85128fe9078d41a3c9eaadb7ca80ad622ca5c462
Deswop <amuadh047@gmail.com>
2022-09-13 19:50:21 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - printing alphabets abc
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
