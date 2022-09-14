bc36dd703a7ee81cd6f38f1947a67390ceac5530
Deswop <amuadh047@gmail.com>
2022-09-13 21:18:24 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - function that prints the sign of a number
+ *
+ * Description: returns 1,0 and  -1
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else (n < 1)
+	{
+		_putchar('-');
+
+		return (-1);
+
+	}
+}
