71973b5c3c6533c9beadc34d604861b0b5f6cdb2
Deswop <amuadh047@gmail.com>
2022-09-14 10:24:29 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,7 @@
  * @c: is the integer that will use for the argument of the function
  * Return: 0
  */
-<<<<<<< HEAD
 int _abs(int c)
-=======
-int _abs(int)
->>>>>>> d61a6490c0ff4bd43cf3a3430963462246425604
 {
 	if (c > 0 || c == 0)
 	{
