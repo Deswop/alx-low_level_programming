fed9155244b69aa3dc7117d2dc1f737cb68117b4
Deswop <amuadh047@gmail.com>
2022-09-14 20:38:43 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table - function for multiplication table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int num, mul, pro;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+	}
+}
