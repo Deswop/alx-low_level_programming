2414114d45cfbcbc48fd765f215a2d6caaff8aaa
Deswop <amuadh047@gmail.com>
2022-09-21 16:27:09 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,22 @@
 #include " main.h"
 
 /**
- * _strncpy: C function that copies a string
+ * _strncat: C function that copies a string
  * @dest: buffer storing the string copy
  * @src: the source string
- * @n: max number of byte copied
+ * @n: integer paarmeter to compare index to
  * Return: return dest
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	while (dest[index++])
+		dest_len++;
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	for (index = 0; src[index] && indx < n;index++)
+		dest[dest_len++]  = src[index];
 
 	return (dest);
-
 }
