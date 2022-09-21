b760af02ce047e3277ff1b5b5335591a4269e986
Deswop <amuadh047@gmail.com>
2022-09-21 07:01:20 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - prototypes
+ * @dest: Destination
+ * @src: Source
+ * @_strcat: catenate string
+ * @_strcpy: string copy
+ * @s1: the first string
+ * @s2: the second srtring
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif /* MAIN_H */
