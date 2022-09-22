4fd8e54fc4e6255f431f57d5f9ac179dd8b990fe
Deswop <amuadh047@gmail.com>
2022-09-22 04:44:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = , dest_len = 0;
+	int index = 0, dest_len = 0;
 
-	whiele (dest[index++])
+	while (dest[index++])
 		dest_len++;
-
-
-
+	
 	for (index = 0; src[index] && index < n; index++)
 	{
 		dest[dest_len++] = src[index];
