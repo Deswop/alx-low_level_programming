9c17dbee407e7aea82125683ac7a62c2822b615b
Deswop <amuadh047@gmail.com>
2022-09-29 07:27:21 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,9 +27,9 @@ void check(char *s, int start, int end, int *flag)
 	if (s[start] <= s[end])
 	{
 		if (s[start] == s[end])
-			*flag += 1;
+			*flag *= 1;
 		else
-			*flag += 0;
+			*flag *= 0;
 		check(s, start + 1, end - 1, flag);
 }
 
